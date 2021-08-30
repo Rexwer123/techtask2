68ccc775e2911bd02bb4c506447bcfffb2077bbc
Rexwer123 <leorexwer@gmail.com>
2021-08-30 15:05:08 +0300

    Delete routers directory

--- routers/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const checkForAlertsController = require('../controllers/checkForAlertsController')
-
-router.get('/', checkForAlertsController)
-
-module.exports = router
